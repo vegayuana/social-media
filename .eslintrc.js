06094ad6de9faead06c0f0b66fcd35aea007bd41
vega <vegayuana@gmail.com>
2021-10-09 13:48:16 +0700

    add dependencies and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "jsx-quotes": 2,
+    "no-trailing-spaces": 2,
+    "no-var": 2,
+    "semi": [2, "always"],
+    "react/prop-types": 2,
+  }
+};
